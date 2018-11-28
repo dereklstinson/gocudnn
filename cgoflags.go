a16b34b3747bc98125bb2e928dbf0c918ace89e0
Derek Stinson <dereklstinson@gmail.com>
2018-11-28 14:58:44 -0500

    changed the cgoflags will have to test

--- cgoflags.go
+++ cgoflags.go
@@ -1,9 +1,5 @@
 package gocudnn
 
-//#cgo LDFLAGS:-lcuda
-//#cgo LDFLAGS: -lcudart
-//#cgo LDFLAGS: -lcudnn
-//
-//#cgo LDFLAGS:-L/usr/local/cuda/lib64 -L/usr/local/cuda/lib
+//#cgo LDFLAGS:-L/usr/local/cuda/lib64 -lcuda -lcudart -lcudnn
 //#cgo CFLAGS: -I/usr/local/cuda/include/
 import "C"
