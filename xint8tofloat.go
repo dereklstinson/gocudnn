64d6f94959ded57e3daa7a958c84b6cdf5ed43be
Derek Stinson <dereklstinson@gmail.com>
2019-02-01 17:45:40 -0500

    moved a flag holder into a struct

--- xint8tofloat.go
+++ xint8tofloat.go
@@ -12,6 +12,7 @@ type XInt8ToFloatD struct {
 	//innerswaploweruper *Kernel
 	//innerswapbatch     *Kernel
 	inttofloatnorm *Kernel
+	dflg           DataTypeFlag
 }
 
 //MakeIntToFloatD will return an IntToFloatD which will do a conversion from ints to float.  It is a good way to save on memory.
@@ -31,21 +32,23 @@ func (x Xtra) MakeIntToFloatD(handle *XHandle) (*XInt8ToFloatD, error) {
 		inttofloatnorm: k2,
 	}, nil
 }
+
+//Int8ToFloat converts an Int8 intput to a FLoat
 func (xint *XInt8ToFloatD) Int8ToFloat(handle *XHandle, xD *TensorD, x *Malloced, yD *TensorD, y *Malloced, normal bool) error {
-	var dflg DataTypeFlag
+
 	xdata, xdims, _, err := xD.GetDescrptor()
 	if err != nil {
 		return err
 	}
-	if xdata != dflg.Int8() {
+	if xdata != xint.dflg.Int8() {
 		return errors.New("Input Data Type Needs to be Int8")
 	}
 	ydata, ydims, _, err := yD.GetDescrptor()
 	if err != nil {
 		return err
 	}
-	if ydata != dflg.Float() {
-		return errors.New("Input Data Type Needs to be Int8")
+	if ydata != xint.dflg.Float() {
+		return errors.New("Output Data Type Needs to be Float")
 	}
 	if findvol(xdims) != findvol(ydims) {
 		return errors.New("Size of xD, and yD tensors need to be the same")
