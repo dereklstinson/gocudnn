18f7edc44afd392e31a1ac091678019601468b70
Derek Stinson <dereklstinson@gmail.com>
2019-03-17 04:07:34 -0400

    made a kern for batching channels

--- kernels/gocudnnxtra.cu
+++ kernels/gocudnnxtra.cu
@@ -941,3 +941,24 @@ extern "C" __global__ void ConcatBackwardNCHW( const int XThreads,
         }
     }
 }
+//MakePlanarImageBatchesUint8 - for this to work all the each batch should have the same amount of channels and all the channels
+//need to be the same size 
+extern "C" __global__ void MakePlanarImageBatchesUint8(const int XThreads, //Should be channel size
+                                                 const int Batches,
+                                                 const int channelsperbatch,
+                                                 const float *Srcs, //all the channels for everything.
+                                                 float *dest)
+{
+    const int batchsize = XThreads*channelsperbatch;
+    for (int i = 0;i<Batches;i++)
+    {
+        for (int j = 0;j<channelsperbatch;j++)
+        {
+            CUDA_GRID_LOOP_X(xIdx, XThreads)
+            {
+               dest[(i*batchsize)+(j*XThreads)+xIdx]=Srcs[(j*XThreads)+xIdx];
+            }
+        }
+    
+    }
+}
