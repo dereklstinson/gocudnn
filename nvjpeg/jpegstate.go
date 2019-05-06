e5f96482ba6948bb032749b690673bcd2a299557
Derek Stinson <dereklstinson@gmail.com>
2019-05-06 15:50:53 -0400

    changed func name and added sync to some funcs

--- nvjpeg/jpegstate.go
+++ nvjpeg/jpegstate.go
@@ -16,8 +16,8 @@ type JpegState struct {
 	j C.nvjpegJpegState_t
 }
 
-//JpegStateCreate creates an initialized decode state
-func JpegStateCreate(h *Handle) (*JpegState, error) {
+//CreateJpegState creates an initialized decode state
+func CreateJpegState(h *Handle) (*JpegState, error) {
 	j := new(JpegState)
 	err := status(C.nvjpegJpegStateCreate(h.h, &j.j)).error()
 	if err != nil {
@@ -43,6 +43,7 @@ func nvjpegJpegStateDestroy(j *JpegState) error {
   and minimum required memory buffer for each plane is nPlaneHeight*nPlanePitch where nPlanePitch >= nPlaneWidth for
   planar output formats and nPlanePitch >= nPlaneWidth*nOutputComponents for interleaved output format.
 
+  Function will perform an s.Sync() before returning.
 
   IN/OUT     h             : Library handle
 
@@ -58,17 +59,24 @@ func nvjpegJpegStateDestroy(j *JpegState) error {
 func (j *JpegState) Decode(h *Handle, data []byte, frmt OutputFormat, dest *Image, s gocu.Streamer) error {
 	d := (*C.uchar)(&data[0])
 	length := len(data)
-	return status(C.nvjpegDecode(h.h, j.j, d, C.size_t(length), frmt.c(), dest.cptr(), stream(s))).error()
+	err := status(C.nvjpegDecode(h.h, j.j, d, C.size_t(length), frmt.c(), dest.cptr(), stream(s))).error()
+	if err != nil {
+		return err
+	}
+	return s.Sync()
 }
 
 /*DecodePhase1 - CPU processing
 
 If error is received restart at phase one
+
+Function will not call an s.Sync(). It will be advised to sync your stream before you go to DecodePhase2.
 */
 func (j *JpegState) DecodePhase1(h *Handle, data []byte, frmt OutputFormat, s gocu.Streamer) error {
 	d := (*C.uchar)(&data[0])
 	length := len(data)
 	return status(C.nvjpegDecodePhaseOne(h.h, j.j, d, C.size_t(length), frmt.c(), stream(s))).error()
+
 }
 
 /*DecodePhase2 -  Mixed processing that requires interaction of both GPU and CPU.
@@ -78,9 +86,12 @@ Any previous call DecodeGPU() with same handle should be finished before this ca
 DecodePhase1 must be ran before DecodePhase2.
 
 If error is received restart at phase one.
+
+Function will not call an s.Sync(). It will be advised to sync your stream before you go to DecodePhase3.
 */
 func (j *JpegState) DecodePhase2(h *Handle, s gocu.Streamer) error {
 	return status(C.nvjpegDecodePhaseTwo(h.h, j.j, stream(s))).error()
+
 }
 
 /*DecodePhase3 - GPU processing.
@@ -88,9 +99,12 @@ func (j *JpegState) DecodePhase2(h *Handle, s gocu.Streamer) error {
 DecodePhase2() must be ran before DecodePhase3().
 
 If error is received restart at phase one.
+
+Function will not call an s.Sync(). It will be advised to sync your stream before you do anything with the dest image.
 */
 func (j *JpegState) DecodePhase3(h *Handle, dest *Image, s gocu.Streamer) error {
 	return status(C.nvjpegDecodePhaseThree(h.h, j.j, dest.cptr(), stream(s))).error()
+
 }
 
 //////////////////////////////////////////////
@@ -125,6 +139,8 @@ IN         data          : Slice of byte slices of input buffers containing the 
 IN/OUT     destinations  : Array of pointers to structure with information about output buffers. len(dest) == len(data)
 
 IN/OUT     s             : gocu.Streamer where to submit all GPU work
+
+Function will perform an s.Sync() before returning.
 */
 func (j *JpegState) DecodeBatched(h *Handle, data [][]byte, dest []*Image, s gocu.Streamer) error {
 	x := make([]*C.uchar, len(data))
@@ -138,29 +154,47 @@ func (j *JpegState) DecodeBatched(h *Handle, data [][]byte, dest []*Image, s goc
 		z[i] = dest[i].cptr()
 	}
 
-	return status(C.nvjpegDecodeBatched(h.h, j.j, &x[0], &y[0], z[0], stream(s))).error()
+	err := status(C.nvjpegDecodeBatched(h.h, j.j, &x[0], &y[0], z[0], stream(s))).error()
+	if err != nil {
+		return err
+	}
+	return s.Sync()
 }
 
 /*DecodeBatchedPhase1 - nvjpegDecodePlanarBatchedCPU should be called [batch_size] times for each image in batch.
 This function is thread safe and could be called by multiple threads simultaneously, by providing
 thread_idx (thread_idx should be less than max_cpu_threads from nvjpegDecodeBatchedInitialize())
 If error is received restart at phase one.
+
+Function will perform an s.Sync() before returning.
 */
 func (j *JpegState) DecodeBatchedPhase1(h *Handle, data []byte, imageidx, threadidx int, s gocu.Streamer) error {
-	return status(C.nvjpegDecodeBatchedPhaseOne(h.h, j.j, (*C.uchar)(&data[0]), C.size_t(len(data)), C.int(imageidx), C.int(threadidx), stream(s))).error()
+	err := status(C.nvjpegDecodeBatchedPhaseOne(h.h, j.j, (*C.uchar)(&data[0]), C.size_t(len(data)), C.int(imageidx), C.int(threadidx), stream(s))).error()
+	if err != nil {
+		return err
+	}
+	return s.Sync()
 }
 
 /*DecodeBatchedPhase2 - nvjpegDecodePlanarBatchedMixed. Any previous call to nvjpegDecodeBatchedGPU() should be done by this point
 
 If error is received restart at phase one.
+
+Function will perform an s.Sync() before returning.
 */
 func (j *JpegState) DecodeBatchedPhase2(h *Handle, s gocu.Streamer) error {
-	return status(C.nvjpegDecodeBatchedPhaseTwo(h.h, j.j, stream(s))).error()
+	err := status(C.nvjpegDecodeBatchedPhaseTwo(h.h, j.j, stream(s))).error()
+	if err != nil {
+		return err
+	}
+	return s.Sync()
 }
 
 /*DecodeBatchedPhase3 - nvjpegDecodePlanarBatchedGPU
 
 If error is received restart at phase one.
+
+Function will perform an s.Sync() before returning.
 */
 func (j *JpegState) DecodeBatchedPhase3(h *Handle, dest []*Image, s gocu.Streamer) error {
 	z := make([]*C.nvjpegImage_t, len(dest))
@@ -168,5 +202,9 @@ func (j *JpegState) DecodeBatchedPhase3(h *Handle, dest []*Image, s gocu.Streame
 
 		z[i] = dest[i].cptr()
 	}
-	return status(C.nvjpegDecodeBatchedPhaseThree(h.h, j.j, z[0], stream(s))).error()
+	err := status(C.nvjpegDecodeBatchedPhaseThree(h.h, j.j, z[0], stream(s))).error()
+	if err != nil {
+		return err
+	}
+	return s.Sync()
 }
